ef46a376ada35fe9d5a4d5671f5a01babc7aa60e
lionelrrivas <lionel.r.rivas@gmail.com>
2023-02-20 08:53:09 -0600

    wrap async function around await

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-try {
-  const token = core.getInput("token");
-  const title = core.getInput("title");
-  const body = core.getInput("body");
-  const assignees = core.getInput("assignees");
+async function run() {
+  try {
+    const token = core.getInput("token");
+    const title = core.getInput("title");
+    const body = core.getInput("body");
+    const assignees = core.getInput("assignees");
 
-  const octokit = new github.getOctokit(token);
-  const response = await octokit.rest.issues.create({
-    ...github.context.repo,
-    title: title,
-    body: body,
+    const octokit = new github.getOctokit(token);
+    const response = await octokit.rest.issues.create({
+      ...github.context.repo,
+      title: title,
+      body: body,
 
-    assignees: assignees ? assignees.split("\n") : undefined,
-  });
+      assignees: assignees ? assignees.split("\n") : undefined,
+    });
 
-  core.setOutput("issue", JSON.stringify(response.data));
-} catch (error) {
-  core.setFailed(error);
+    core.setOutput("issue", JSON.stringify(response.data));
+  } catch (error) {
+    core.setFailed(error);
+  }
 }
+
+run();
